ccdf4e1b7bfe90e20ab35dff8c7c9006f65c8f0d
Kara Marie Rawson <rawsonkara@gmail.com>
2017-11-12 08:02:26 -0500

    implement boilerplate three js code to draw a green box

--- client/src/renderer/index.js
+++ client/src/renderer/index.js
@@ -2,7 +2,14 @@
 
 import Game from "./game";
 
+function init() { 
+    document.body.scrollTop = 0;
+    document.body.style.overflow = "hidden";
+    document.body.style.margin = "0px";
+}
+
 function main() {
+	init();
 	let game = new Game();
 }
 
