ff9580fdf9856660c2cf65bb78871be2dd3825b2
Kara Marie Rawson <rawsonkara@gmail.com>
2017-11-13 04:57:25 -0500

    added MainLoop logic

--- client/src/renderer/game.js
+++ client/src/renderer/game.js
@@ -1,33 +1,48 @@
 "use strict";
 
 import * as THREE from 'three';
+import * as MainLoop from 'mainloop.js'
 
 class Game {
 	constructor() {
-		var scene = new THREE.Scene();
-		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+		// var scene = new THREE.Scene();
+		// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
+		// var renderer = new THREE.WebGLRenderer();
+		// renderer.setSize( window.innerWidth, window.innerHeight );
+		// document.body.appendChild( renderer.domElement );
 
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		var cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+		// var cube = new THREE.Mesh( geometry, material );
+		// scene.add( cube );
 
-		camera.position.z = 5;
+		// camera.position.z = 5;
 
-		var animate = function () {
-			requestAnimationFrame( animate );
+		// var animate = function () {
+		// 	requestAnimationFrame( animate );
 
-			cube.rotation.x += 0.1;
-			cube.rotation.y += 0.1;
+		// 	cube.rotation.x += 0.1;
+		// 	cube.rotation.y += 0.1;
 
-			renderer.render(scene, camera);
-		};
+		// 	renderer.render(scene, camera);
+		// };
 
-		animate();
+		// animate();
+		MainLoop.setBegin(this.begin).setUpdate(this.update).setDraw(this.draw).setEnd(this.end).start();
+	}
+
+	begin(timestamp, delta) {
+	}
+
+	update(delta) {
+	}
+
+	draw(interpolationPercentage) {
+	}
+
+	end(fps, panic) {
+		if(panic) MainLoop.resetFrameDelta() 
 	}
 }
 
