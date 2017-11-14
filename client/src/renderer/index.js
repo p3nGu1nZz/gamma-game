67b3b4a772e70708d2cbd21aec9c30dbf9a07558
Kara Marie Rawson <rawsonkara@gmail.com>
2017-11-14 02:57:11 -0500

    more documentation

--- client/src/renderer/index.js
+++ client/src/renderer/index.js
@@ -2,9 +2,13 @@
 
 import Game from "./game";
 
+// global variables =? all functions can access
 window.isDev = process.env.NODE_ENV !== "production";
 window.game = null;
 
+/*
+ * our function that is used to setup the window
+ */
 function init() {
   window.game = new Game();
   Game.container.scrollTop = 0;
@@ -12,14 +16,21 @@ function init() {
   Game.container.style.margin = "0px";
 }
 
+/*
+ * used to run a game in the window
+ */
 function run() {
   window.game.load(window.game.state);
   window.game.start(window.game.state);
 }
 
+/*
+ * entry point for game
+ */
 function main() {
   init();
   run();
 }
 
+// do it!!!
 main();
