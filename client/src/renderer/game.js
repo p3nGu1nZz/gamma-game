21912f42215ec977ce22ac975dfdc87de79c9101
Kara Marie Rawson <rawsonkara@gmail.com>
2017-11-14 02:54:13 -0500

    code cleanup and documentation

--- client/src/renderer/game.js
+++ client/src/renderer/game.js
@@ -8,32 +8,38 @@ import * as Stats from "stats.js";
  * main class that is used to control and render the game in the window
  */
 class Game {
+  /*
+	 * creates our game to be played
+	 */
   constructor() {
     // game state for dependency injection
     let state = {};
 
-    // load the game's state and start the game using the loaded state
-    load(state);
-    start(state);
-
+    // loads the game objects before starting the main loop
     function load(state) {
       if (isDev) createStats(state);
 
+      // our scene to load stuff into and camera to view it
       let scene = new THREE.Scene();
-      let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      let camera = new THREE.PerspectiveCamera(75, Game.windowX / Game.windowY, 0.1, 1000);
 
+      // create our renderer for the scene
       let renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+      renderer.setSize(Game.windowX, Game.windowY);
+      Game.container.appendChild(renderer.domElement);
 
+      // create cube game object
       let geometry = new THREE.BoxGeometry(1, 1, 1);
       let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
       let cube = new THREE.Mesh(geometry, material);
 
+      // add stuff into our scene
       scene.add(cube);
 
+      // move our camera
       camera.position.z = 5;
 
+      // update the game state with loaded objects
       Object.assign(state, {
         scene,
         camera,
@@ -44,6 +50,9 @@ class Game {
       });
     }
 
+    /*
+     * starts the actual game's main loop and configures loop functions
+     */
     function start(state) {
       state.mainLoop = MainLoop.setBegin(begin)
         .setUpdate(update)
@@ -52,14 +61,33 @@ class Game {
         .start();
     }
 
+    /*
+     * pauses the game and main loop
+     */
     function pause() {
       state.mainLoop.stop();
+      state.paused = true;
     }
 
+    /*
+     * resumes the game from being paused
+     */
     function resume() {
       state.mainLoop.start();
+      state.paused = false;
     }
 
+    /* 
+     * helper function to determin if the game is paused
+     */
+    function isPaused() {
+      return state.paused;
+    }
+
+    /*
+     * called first before update or draw in our main loop. used to set up
+     * the scene for updating
+     */
     function begin(timestamp, delta) {
       if (isDev) {
         state.fpsStats.begin();
@@ -67,6 +95,9 @@ class Game {
       }
     }
 
+    /*
+     * called every frame, or more for prediciton. used for AI and physics
+     */
     function update(delta) {
       if (isDev) state.updateStats.begin();
 
@@ -75,16 +106,22 @@ class Game {
       if (isDev) state.updateStats.end();
     }
 
+    /*
+     * this function is called every frame to render objects
+     */
     function draw(interpolationPercentage) {
       if (isDev) state.drawStats.begin();
 
-      state.cube.rotation.x += 0.1;
-      state.cube.rotation.y += 0.1;
+      state.cube.rotation.x += 0.05;
+      state.cube.rotation.y += 0.05;
       state.renderer.render(state.scene, state.camera);
 
       if (isDev) state.drawStats.end();
     }
 
+    /*
+     * the last function called in our main loop. used for cleanup
+     */
     function end(fps, panic) {
       if (panic) MainLoop.resetFrameDelta();
       if (isDev) {
@@ -93,39 +130,47 @@ class Game {
       }
     }
 
+    /*
+     * creates the dev stats to monitor performance
+     */
     function createStats() {
+      // calculate our current interpolated fps => update + draw
       let fpsStats = new Stats();
       fpsStats.showPanel(0);
       fpsStats.dom.style.opacity = "0.69";
       fpsStats.dom.style.position = "absolute";
       fpsStats.dom.style.top = "4px";
       fpsStats.dom.style.left = "4px";
-      document.body.appendChild(fpsStats.dom);
+      Game.container.appendChild(fpsStats.dom);
 
+      // calculate the total update time of the game objects
       let updateStats = new Stats();
       updateStats.showPanel(1);
       updateStats.dom.style.opacity = "0.69";
       updateStats.dom.style.position = "absolute";
       updateStats.dom.style.top = "4px";
       updateStats.dom.style.left = "84px";
-      document.body.appendChild(updateStats.dom);
+      Game.container.appendChild(updateStats.dom);
 
+      // calculate the total draw time of renderer
       let drawStats = new Stats();
       drawStats.showPanel(1);
       drawStats.dom.style.opacity = "0.69";
       drawStats.dom.style.position = "absolute";
       drawStats.dom.style.top = "4px";
       drawStats.dom.style.left = "168px";
-      document.body.appendChild(drawStats.dom);
+      Game.container.appendChild(drawStats.dom);
 
+      // used to track game memory heap usuage
       let memStats = new Stats();
       memStats.showPanel(2);
       memStats.dom.style.opacity = "0.69";
       memStats.dom.style.position = "absolute";
       memStats.dom.style.top = "4px";
       memStats.dom.style.left = "252px";
-      document.body.appendChild(memStats.dom);
+      Game.container.appendChild(memStats.dom);
 
+      // update the game state
       Object.assign(state, {
         fpsStats,
         updateStats,
@@ -134,12 +179,37 @@ class Game {
       });
     }
 
-    //public
+    /*
+     * public
+     */
     return {
       state: state,
+      load: load,
+      start: start,
       pause: pause,
       resume: resume,
     };
+  }
+
+  /*
+   * Global static reference to what we put the game into
+   */
+  static get container() {
+    return document.body;
+  }
+
+  /*
+   * the horizontal width of our window in pixels
+   */
+  static get windowX() {
+    return window.innerWidth;
+  }
+
+  /*
+   * the vertical height of our window in pixels
+   */
+  static get windowY() {
+    return window.innerHeight;
   }
 }
 
--- client/src/renderer/index.js
+++ client/src/renderer/index.js
@@ -6,14 +6,20 @@ window.isDev = process.env.NODE_ENV !== "production";
 window.game = null;
 
 function init() {
-  document.body.scrollTop = 0;
-  document.body.style.overflow = "hidden";
-  document.body.style.margin = "0px";
+  window.game = new Game();
+  Game.container.scrollTop = 0;
+  Game.container.style.overflow = "hidden";
+  Game.container.style.margin = "0px";
+}
+
+function run() {
+  window.game.load(window.game.state);
+  window.game.start(window.game.state);
 }
 
 function main() {
   init();
-  window.game = new Game();
+  run();
 }
 
 main();
